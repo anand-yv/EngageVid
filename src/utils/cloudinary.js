c4bd72ee14ea04a497870e8148bd6054bfa8ecdb
anand-yv <anand93056ad@gmail.com>
2024-05-23 16:52:13 +0530

    Added cloudnary service to upload files and implemented first middleware for uploading file on server first then to cloud
    The file on mutler is only stored for short duration then it is remove from storage and we are usin disk storage in mutler
    
    Day - 02/23-05-2024/Thursday

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+// Configuration
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: CLOUDINARY_API_KEY,
+    api_secret: CLOUDINARY_API_SECRET,
+});
+
+const uplodaOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null;
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto",
+        })
+        // File has been uploaded successfully
+        console.log("File has been uploaded successfull at : ", response.url);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath); // removing the locally saved temporary file as the upload operation got failed
+        return null;
+    }
+}
+
+export { uplodaOnCloudinary };
