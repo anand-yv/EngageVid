ff91100a97a68d1f0a7cc842b797820fb0ede064
anand-yv <anand93056ad@gmail.com>
2024-05-23 22:02:34 +0530

    Added Routes and Controller for Users
    Done through app which use middleware for routes
    
    Day - 02/23-05-2024

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import cookiesParser from "cookies-parser";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -12,7 +12,13 @@ app.use(cors({
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
-app.use(cookiesParser());
+app.use(cookieParser());
 
+
+// Importing Routes
+import userRouter from "./routes/user.routes.js";
+
+// Routes Declaration
+app.use("/api/v1/users", userRouter);
 
 export { app };
--- src/index.js
+++ src/index.js
@@ -5,10 +5,6 @@ dotenv.config({ path: './env' })
 
 connectDB()
     .then(() => {
-        app.on(error, (error) => {
-            console.log("Error : ", error);
-            throw error;
-        })
         app.listen(process.env.PORT || 8000, () => {
             console.log(`Server is listening at port: ${process.env.PORT}`)
         })
